8873d0f4a8c9cf30b676636c51d2e912c0020bc8
DaedricOrc <izaiah.donaldsonashe@horowhenua.school.nz>
2026-03-24 12:27:41 +1300

    created player laser

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export var speed:int = 10
+@onready var player_laser_scene = preload("res://scenes/player_laser.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,3 +17,11 @@ func _process(delta: float) -> void:
 		position.x += speed
 	elif Input.is_action_pressed("player_up"):
 		position.y -= speed
+		
+	position.x = clampi(position.x, 19, 1133)
+	position.y = clampi(position.y, 25, 623)
+
+	if Input.is_action_pressed("player_fire"):
+		var player_laser = player_laser_scene.instantiate()
+		player_laser.position = position
+		get_parent().add_child(player_laser)
