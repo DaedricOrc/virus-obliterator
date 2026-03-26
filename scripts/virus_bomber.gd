c32ef082abf5feba5848f55cae50d0ef85d0e373
DaedricOrc <izaiah.donaldsonashe@horowhenua.school.nz>
2026-03-27 08:59:45 +1300

    created enemy and enemy laser

--- scripts/virus_bomber.gd
+++ scripts/virus_bomber.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @export var speed: int = 4
 
+@onready var enemy_laser_scene = preload("res://scenes/virus_bombers_bomb.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,3 +12,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	position.x -= speed
+
+
+func _on_timer_timeout() -> void:
+	var enemy_laser = enemy_laser_scene.instantiate()
+	enemy_laser.position = position
+	get_parent().add_child(enemy_laser)
